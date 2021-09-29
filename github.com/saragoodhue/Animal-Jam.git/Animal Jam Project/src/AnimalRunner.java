85182d48245628696cb93126f44dd71d78fc1472
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2021-09-29 12:10:40 -0600

    working on the map, need to add on XP and health

--- github.com/saragoodhue/Animal-Jam.git/Animal Jam Project/src/AnimalRunner.java
+++ github.com/saragoodhue/Animal-Jam.git/Animal Jam Project/src/AnimalRunner.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class AnimalRunner
 	{
 		static ArrayList<AnimalJam> player = new ArrayList<AnimalJam>();
+
 		public static void main(String[] args)
 			{
 				player.add(new AnimalJam("Seal", "Clownfish", "Pink"));
@@ -10,9 +12,41 @@ public class AnimalRunner
 				player.add(new AnimalJam("Rhino", "Pigeon", "Black"));
 				player.add(new AnimalJam("Bunny", "BumbleBee", "Purple"));
 				player.add(new AnimalJam("Tiger", "Wild Dog", "Orange"));
-				player.add(new AnimalJam("Koala", "Owl", "Red"));
+				player.add(new AnimalJam("Koala", "LadyBug", "Red"));
 				player.add(new AnimalJam("Panda", "Monkey", "White"));
 				player.add(new AnimalJam("Penguin", "Krill", "Yellow"));
+
+				playerCharacter();
+				beginJourney();
+
+			}
+
+		public static void playerCharacter()
+			{
+				Random playerCharacter = new Random();
+				Random playerPet = new Random();
+				Random playerColor = new Random();
+				int upperbound = 8;
+				int characterRandom = playerCharacter.nextInt(upperbound);
+				int petRandom = playerPet.nextInt(upperbound);
+				int colorRandom = playerColor.nextInt(upperbound);
+
+				System.out.println("Welcome to Animal Jam! (revised version)");
+				System.out.println("You will be randomly assigned an avatar");
+				System.out.println("You will be a " + player.get(characterRandom).getCharacter() + ", your pet is a "
+						+ player.get(petRandom).getPets() + ", and your color is "
+						+ player.get(colorRandom).getColor());
+				
+				System.out.println();
+
+			}
+
+		public static void beginJourney()
+			{
+				int experiencePoints = 0;
+				int health = 100;
+				
+				
 
 			}
 
--- github.com/saragoodhue/Animal-Jam.git/Animal Jam Project/src/Map.java
+++ github.com/saragoodhue/Animal-Jam.git/Animal Jam Project/src/Map.java
@@ -13,44 +13,52 @@ public class Map
 				serapiaForest();
 				templeOfZios();
 			}
-		
+
 		public static void jamaaTownship()
-		{
-			
-		}
-		
+			{
+				System.out.println("Welcome to Jamaa Township, the first stop in your journey");
+
+			}
+
 		public static void appondale()
 			{
-				
+				System.out.println("Welcome to Appondale, the second stop in your journey");
+
 			}
 
 		public static void kimbaraOutback()
 			{
-				
+				System.out.println("Welcome to Kimbara Outback, the third stop in your journey");
+
 			}
-	
+
 		public static void mtShiveer()
 			{
-				
+				System.out.println("Welcome to Mt. Shiveer, the fourth stop in your journey");
+
 			}
-	
+
 		public static void coralCanyons()
 			{
-				
+				System.out.println("Welcome to Coral Canyons, the fifth stop in your journey");
+
 			}
-	
+
 		public static void crystalSands()
 			{
-				
+				System.out.println("Welcome to Crystal Sands, the sixth stop in your journey");
+
 			}
-	
+
 		public static void serapiaForest()
 			{
-				
+				System.out.println("Welcome to Serapia Forest, the seventh stop in your journey");
+
 			}
-	
+
 		public static void templeOfZios()
 			{
-				
+				System.out.println("Welcome to the Lost Temple of Zios, the last stop in your journey");
+
 			}
 	}
